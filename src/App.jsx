8317e2cba05b5760e7442c3ff8dafb4e0d66a2d2
Bruno Torres <brunotorres@bemol.com.br>
2020-07-24 14:38:44 -0400

    cards adjusts

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ export default () => (
     <div className="app">
         <h1>React Fundamentals</h1>
         <div className="cards">
-            <Card title="#04 - Random">
+            <Card title="#04 - Random" color="#93b5e1">
                 <Random min={1} max={61} />
             </Card>
-            <Card title="#03 - Fragment">
+            <Card title="#03 - Fragment" color="#3b2e5a">
                 <Fragment />
             </Card>
-            <Card title="#02 - Component With Params">
+            <Card title="#02 - Component With Params" color="#3282b8">
                 <ComponentWithParams title="Student Status" student="Bruno Torres" note={9.3} />
             </Card>
-            <Card title="#01 - First Component">
+            <Card title="#01 - First Component" color="#d3de32">
                 <FirstComponent></FirstComponent>
             </Card>
         </div>
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -4,8 +4,13 @@ import React from 'react'
 
 export default props => {
 
+    const card_style = {
+        backgroundColor: props.color || '#F00',
+        borderColor: props.color || '#F00'
+    }
+
     return (
-        <div className="card">
+        <div className="card" style={card_style}>
             <div className="title">{props.title}</div>
             <div className="content">
                 {props.children}
